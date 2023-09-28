830664bd5d7a2a66029f36a79014242228ce8296
CrazyFishboy <gillum0927@gmail.com>
2023-09-28 10:16:19 -0700

    gets input from user and checks if their answer is correct

--- main.cpp
+++ main.cpp
@@ -5,14 +5,38 @@
 #include <random>
 #include <string>
 
+std::string cleanInput(const std::string &in);
 
 int main(){
-    unsigned max = 16*16*16*16;
+    unsigned max = 16;
     std::srand(std::time(0));
     int rand = std::rand() % max;
     std::cout << "Hexadecimal: 0x" << std::hex << rand << std::endl;
     std::cout << "Decimal?: ";
+    std::string input;
+    std::getline(std::cin, input);
+    input = cleanInput(input);
+    if(input.size() > 0){
+        if(std::stoi(input) == rand){
+            std::cout << "Correct!" << std::endl;
+        } else {
+            std::cout << "Incorrect. The answer was: " << rand << std::endl;
+        }
+    } else {
+        std::cout << "Please only enter digits (0-9)." << std::endl;
+    }
     
     
     return 0;
+}
+
+
+std::string cleanInput(const std::string &in){
+    std::string replacement;
+    for(unsigned i = 0; i < in.size(); ++i){
+        if(in[i] >= '0' && in[i] <= '9'){
+            replacement += in[i];
+        }
+    }
+    return replacement;
 }
